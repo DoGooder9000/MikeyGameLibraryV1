a2aa07e40b5fe2d2d57c2f675c846571aeece56a
DoGooder9000 <mike@rogness.net>
2023-11-05 17:39:48 -0600

    Made Colors and Basic Drawing Functions Along With Window Updating

--- Code/MikeyGames.py
+++ Code/MikeyGames.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.gfxdraw
 import time
 
 
@@ -6,6 +7,33 @@ import time
 
 QUIT = pygame.QUIT
 KEYDOWN = pygame.KEYDOWN
+
+#Basic Color Tuples
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+#Colors
+class Color:
+    def __init__(self, rgb: (int, int, int)):
+        self.rgb = rgb
+        self.r = rgb[0]
+        self.g = rgb[1]
+        self.b = rgb[2]
+
+#Basic Drawing Functions#
+class Draw:
+    def __init__(self):
+        self.Color = Color(white)
+
+    def Window_Fill(self, window):
+        window.fill(self.Color.rgb)
+        
+    
+    def Point(self, window, x: int, y: int):
+        pygame.gfxdraw.pixel(window, x, y, self.Color.rgb)
+
+    def Line(self, window, p1: (int, int), p2: (int, int)):
+        pygame.gfxdraw.line(window, p1[0], p1[1], p2[0], p2[1], self.Color.rgb)
 
 
 def init():
@@ -17,10 +45,13 @@ def close():
 def wait(sec: float):
     time.sleep(sec)
 
-def window(width: int, height: int, name: str = None):
+def Window(width: int, height: int, name: str = None):
     window = pygame.display.set_mode((width, height))
     if name: pygame.display.set_caption(name)
     return window
 
 def Get_Event():
     return pygame.event.get()
+
+def Update():
+    pygame.display.update()
--- Code/test.py
+++ Code/test.py
@@ -1,6 +1,8 @@
 from MikeyGames import *
 
-window(600, 600, "Test")
+window = Window(600, 600, "Test")
+
+Drawer = Draw()
  
 running = True
 
@@ -8,5 +10,11 @@ while running:
     for event in Get_Event():
         if event.type == QUIT:
             running = False
+    
+    Drawer.Color = Color(black)
+    Drawer.Window_Fill(window)
+    Drawer.Color = Color(white)
+    Drawer.Line(window, (0, 0), (100, 100))
+    Update()
 
 close()
